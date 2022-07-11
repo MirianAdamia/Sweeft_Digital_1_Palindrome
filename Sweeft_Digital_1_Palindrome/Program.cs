bf8b901b49ad6a48f3d47f39bd8b3e1a40031213
Nen <Nen@DESKTOP-EPGE2L2>
2022-07-11 17:57:10 +0400

    Add project files.

---
+++ Sweeft_Digital_1_Palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿static string sPalindrome(string PalindromeInputString ) {
+string _reversestr = string.Empty;
+
+
+if (PalindromeInputString != null)
+{
+    for (int i = PalindromeInputString.Length - 1; i >= 0; i--)
+    {
+        _reversestr += PalindromeInputString[i].ToString();
+    }
+    if (_reversestr == PalindromeInputString)
+    {
+        Console.WriteLine("String is Palindrome");
+    }
+    else
+    {
+        Console.WriteLine("String is not Palindrome");
+    }
+}
+
+    return _reversestr;
+}
+
+
+
+Console.Write("Enter a string : ");
+string Palindrome = Console.ReadLine();
+sPalindrome(Palindrome);
